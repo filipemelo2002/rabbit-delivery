cf110f9822f82d306b7ead9fea5811cd4f38151a
Filipe Melo <filipe.melo@liferay.com>
2022-11-13 11:30:04 -0300

    remove cached files

--- order-service/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./server"));
-const utils_1 = require("./utils");
-const PORT = process.env.PORT || 3000;
-server_1.default.listen(PORT, () => utils_1.logger.info(`Server running at ${PORT}`));
